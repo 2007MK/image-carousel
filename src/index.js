110ad6c66eb9399c63a72acca88ef840386e1e90
Mehul <mehulk2007@gmail.com>
2025-07-14 07:52:22 +0000

    Navigation dots pending

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./styles.css";
-import { slideController } from "./frame";
-
-slideController.hideAllSlides();
-
-slideController.setCurrentSlide(1);
-
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/image-carousel.js",
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
